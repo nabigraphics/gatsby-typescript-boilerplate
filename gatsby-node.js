d8d501adf72213087488d3e7b7c572b5905d370f
nabigraphics <hbh2692@naver.com>
2019-09-30 12:36:42 +0900

    Fix react-hot-loader issue with use react v16.8.0^

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,17 @@
 const path = require("path");
 const SRC_DIR = path.resolve(__dirname, "src");
 
-exports.onCreateWebpackConfig = ({ actions }) => {
+exports.onCreateWebpackConfig = ({ stage, getConfig, actions }) => {
+  const config = getConfig();
+
+  // fix react-hot-loader issue with use react v16.8.0^
+  if (stage.startsWith("develop") && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "react-dom": "@hot-loader/react-dom"
+    };
+  }
+
   actions.setWebpackConfig({
     resolve: {
       modules: [SRC_DIR, "node_modules"]
