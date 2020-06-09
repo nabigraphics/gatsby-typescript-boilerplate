7d7f52cc0ddeecab0c40bc9944e57cd8b6998628
Jusung Hwang(nabigraphics) <hbh2692@naver.com>
2020-06-09 16:14:38 +0900

    Set config for eslint and typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,25 @@
 module.exports = {
   plugins: [
+    {
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ["develop"],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
     "gatsby-plugin-typescript",
     "gatsby-plugin-sass",
     "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-layout",
       options: {
-        component: require.resolve("./src/components/layouts/index.tsx")
-      }
-    }
-  ]
+        component: require.resolve("./src/components/layouts/index.tsx"),
+      },
+    },
+  ],
 };
